ce5b1c7657de56efd08d604a203a2caf89cc753d
killshotxd <hassanansari211@gmail.com>
2022-08-06 10:14:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import Services from "./Services";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <About />
+      <Services />
+      <Contact />
+    </>
+  );
+};
+
+export default App;
