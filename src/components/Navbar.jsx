93d407b06190be9c3c8089413f30c91ed07d532b
killshotxd <hassanansari211@gmail.com>
2022-08-09 10:13:41 +0530

    finalize

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import { Fade } from "react-reveal";
+import { CgMenu, CgCloseR } from "react-icons/cg";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   const Nav = styled.nav`
     .navbar-list {
       display: flex;
       gap: 4.8rem;
+
       li {
         list-style: none;
+
         .navbar-link {
           &:link,
           &:visited {
@@ -19,6 +24,7 @@ const Navbar = () => {
             color: ${({ theme }) => theme.colors.black};
             transition: color 0.3s linear;
           }
+
           &:hover,
           &:active {
             color: ${({ theme }) => theme.colors.helper};
@@ -26,25 +32,31 @@ const Navbar = () => {
         }
       }
     }
+
     .mobile-navbar-btn {
       display: none;
+
       .close-outline {
         display: none;
       }
     }
+
     .mobile-navbar-btn[name="close-outline"] {
       display: none;
     }
+
     @media (max-width: ${({ theme }) => theme.media.mobile}) {
       .mobile-navbar-btn {
         display: inline-block;
         z-index: 999;
         border: ${({ theme }) => theme.colors.black};
+
         .mobile-nav-icon {
           font-size: 4.2rem;
           color: ${({ theme }) => theme.colors.black};
         }
       }
+
       /* hide the original nav menu  */
       .navbar-list {
         width: 100vw;
@@ -53,20 +65,25 @@ const Navbar = () => {
         top: 0;
         left: 0;
         background-color: #fff;
+
         display: flex;
         justify-content: center;
         align-content: center;
         flex-direction: column;
         text-align: center;
+
         transform: translateX(100%);
+
         visibility: hidden;
         opacity: 0;
+
         li {
           .navbar-link {
             &:link,
             &:visited {
               font-size: 4.2rem;
             }
+
             &:hover,
             &:active {
               color: ${({ theme }) => theme.colors.helper};
@@ -74,6 +91,7 @@ const Navbar = () => {
           }
         }
       }
+
       .active .mobile-nav-icon {
         display: none;
         font-size: 4.2rem;
@@ -83,9 +101,11 @@ const Navbar = () => {
         color: ${({ theme }) => theme.colors.black};
         z-index: 9999;
       }
+
       .active .close-outline {
         display: inline-block;
       }
+
       .active .navbar-list {
         visibility: visible;
         opacity: 1;
@@ -97,35 +117,59 @@ const Navbar = () => {
 
   return (
     <Nav>
-      <Fade top>
-        <div className="menu-icon">
-          <ul className="navbar-list">
-            <li>
-              <NavLink className="navbar-link" to="/">
-                Home
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink className="navbar-link" to="/about">
-                About
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink className="navbar-link" to="/services">
-                Projects
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink className="navbar-link" to="/contact">
-                Contact
-              </NavLink>
-            </li>
-          </ul>
+      <div className={openMenu ? "menuIcon active" : "menuIcon"}>
+        <ul className="navbar-list">
+          <li>
+            <NavLink
+              className="navbar-link"
+              onClick={() => setOpenMenu(false)}
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="navbar-link"
+              onClick={() => setOpenMenu(false)}
+              to="/about"
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="navbar-link"
+              onClick={() => setOpenMenu(false)}
+              to="/services"
+            >
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="navbar-link"
+              onClick={() => setOpenMenu(false)}
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+        {/* //nav icon */}
+        <div className="mobile-navbar-btn">
+          <CgMenu
+            name="menu-outline"
+            className="mobile-nav-icon"
+            onClick={() => setOpenMenu(true)}
+          />
+          <CgCloseR
+            name="close-outline"
+            className="close-outline mobile-nav-icon"
+            onClick={() => setOpenMenu(false)}
+          />
         </div>
-      </Fade>
+      </div>
     </Nav>
   );
 };
